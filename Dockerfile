bdc794dcfe9eadc6335ebc87834cfc84a63a23a7
Duog Lee <duoglee@0x1115.com>
2026-02-21 21:30:20 +0700

    Add GPL license headers to multiple files and update copyright year

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,19 @@
+# uploader - A tool to upload files to various cloud storage providers
+# Copyright (C) 2026 0x1115 Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 FROM golang:1.22-alpine AS build
 WORKDIR /src
 RUN apk add --no-cache ca-certificates
--- internal/model/file.go
+++ internal/model/file.go
@@ -1,3 +1,21 @@
+/**
+uploader - A simple file upload server with S3-compatible storage support
+Copyright (C) 2026 0x1115 Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package model
 
 type FileRecord struct {
@@ -9,4 +27,3 @@ type FileRecord struct {
 	ObjectKey   string
 	CreatedAt   string
 }
-
